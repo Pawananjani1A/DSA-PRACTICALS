b4d3a18d78ce2614478912f5ebc1d83e91e8891e
Pawananjani1A <pawananjanimth1@gmail.com>
2020-09-23 13:03:13 +0530

    Add keyboard inputs,images and comtrol movements

--- Snakes Game/snake.js
+++ Snakes Game/snake.js
@@ -14,7 +14,17 @@ function init()
     pen = canvas.getContext('2d');
     cs = 66;
     game_over = false;
+    score = 5;
 
+    // create a image object for food
+    food_img = new Image();
+    food_img.src = "assets/apple.png";
+   
+    trophy = new Image();
+    trophy.src = "assets/trophy.png";
+
+    food = getRandomFood();
+    
     snake = {
         init_len: 5,
         color: "blue",
@@ -37,26 +47,97 @@ function init()
 
         updateSnake: function()
         {
-          console.log("updating snake");
-            this.cells.pop();
+        //   console.log("updating snake according to the direction property");
+    //    check if the snake has eaten food, increase the length of the snake and
+    // generate new food object
             var headX = this.cells[0].x;
             var headY = this.cells[0].y;
 
-            var X = headX + 1;
-            var Y = headY;
-            this.cells.unshift({ x: X, y: Y });
+            if(headX == food.x && headY==food.y)
+            {
+                // console.log("Food eaten");
+                food = getRandomFood();
+                score++;
+
+            }
+            else
+            {
+                this.cells.pop();
+            }
+
+            var nextX,nextY;
+
+            if(this.direction=="right")
+            {
+                nextX = headX + 1;
+                nextY = headY;
+            }
+            else if(this.direction=="left")
+            {
+                nextX = headX-1;
+                nextY = headY;
+            }
+            else if(this.direction=="down")
+            {
+                nextX = headX;
+                nextY = headY+1;
+            }
+            else if(this.direction=="up")
+            {
+              nextX = headX;
+              nextY = headY-1;
+            }
+            
+            this.cells.unshift({ x: nextX, y: nextY });
+
+            /*Write a logic that prevents snake from going out*/
+            //Find out the last coordinate(boundaries)
+            var last_x = Math.round(W / cs);
+            var last_y = Math.round(H / cs);
+
+            if (this.cells[0].y < 0 || this.cells[0].x < 0 || this.cells[0].x > last_x || this.cells[0].y > last_y) {
+
+                game_over = true;
+            }
+
 
         },
 
     };
 
     snake.createSnake();
+
+    function keyPressed(e)
+    {
+        //  console.log("Key pressed",e.key);
+
+        // Conditional statements
+        if(e.key=="ArrowRight") snake.direction="right";
+        else if(e.key=="ArrowLeft") snake.direction="left";
+        else if(e.key=="ArrowDown") snake.direction="down";
+        else if(e.key=="ArrowUp") snake.direction="up";
+        else alert("Press only navigation keys");
+
+
+    }
+
+    // Add a Event Listener on the Document object
+    document.addEventListener('keydown',keyPressed);
+
 }
 
 function draw()
 {   
     pen.clearRect(0,0,W,H);
     snake.drawSnake();
+
+    pen.fillStyle =food.color;
+    pen.drawImage(food_img,food.x*cs,food.y*cs,cs,cs);
+    
+    pen.drawImage(trophy,18,20,cs,cs);
+    pen.fillStyle = "blue";
+    pen.font = "20px Roboto";
+    pen.fillText(score,50,50);
 }
 
 function update()
@@ -64,13 +145,30 @@ function update()
 //   console.log("In update");
  snake.updateSnake();
     
+}
 
+function getRandomFood()
+{
+    var foodX = Math.round(Math.random()*(W-cs)/cs);
+    var foodY = Math.round(Math.random()*(H - cs)/cs);
+
+    var food = {
+        x:foodX,
+        y:foodY,
+        color:"red",
+    };
+  return food;
 }
 
 function gameLoop()
 { 
 //   console.log("In gameLoop");
-    if (game_over === true) clearInterval(f);
+    if (game_over == true)
+    {
+        clearInterval(f);
+        alert("Game Over !");
+        return;
+    }
   draw();
   update();
 }
