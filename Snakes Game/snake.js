b25f28ecb2ce36170131e86a69de95ff6db6f530
Pawananjani1A <pawananjanimth1@gmail.com>
2020-09-23 11:58:42 +0530

    Snake Game - Add basic movement

--- Snakes Game/snake.js
+++ Snakes Game/snake.js
@@ -34,27 +34,36 @@ function init()
                 pen.fillRect(this.cells[i].x*cs, this.cells[i].y*cs, cs-2, cs-2);
             }
         },
+
+        updateSnake: function()
+        {
+          console.log("updating snake");
+            this.cells.pop();
+            var headX = this.cells[0].x;
+            var headY = this.cells[0].y;
+
+            var X = headX + 1;
+            var Y = headY;
+            this.cells.unshift({ x: X, y: Y });
+
+        },
+
     };
 
     snake.createSnake();
 }
 
 function draw()
-{
+{   
+    pen.clearRect(0,0,W,H);
     snake.drawSnake();
-     
-
-
 }
 
 function update()
 {
 //   console.log("In update");
-rect.x += rect.speed;
-if(rect.x>W-rect.w || rect.x<0)
-{
-   rect.speed *= -1;
-}
+ snake.updateSnake();
+    
 
 }
 
